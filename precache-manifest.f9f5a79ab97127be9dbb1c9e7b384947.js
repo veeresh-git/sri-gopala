0ea1fe39fb35708859a2b5b437c0ee69b4679ff7
veeresh-git <veereshv537@gmail.com>
2020-02-13 18:53:41 +0530

    Updates

---
+++ precache-manifest.f9f5a79ab97127be9dbb1c9e7b384947.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64f3f5a8fc883b69a1230635b7b9832d",
+    "url": "/sri-gopala/index.html"
+  },
+  {
+    "revision": "b14c4594886cf5811997",
+    "url": "/sri-gopala/static/css/main.1a1646a0.chunk.css"
+  },
+  {
+    "revision": "f5bd15c2c5cde5a86396",
+    "url": "/sri-gopala/static/js/2.26c2d070.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/sri-gopala/static/js/2.26c2d070.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b14c4594886cf5811997",
+    "url": "/sri-gopala/static/js/main.b1c0ef30.chunk.js"
+  },
+  {
+    "revision": "a76957a7af9dd3822079",
+    "url": "/sri-gopala/static/js/runtime-main.582e4ced.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sri-gopala/static/media/logo.5d5d9eef.svg"
+  }
+]);
